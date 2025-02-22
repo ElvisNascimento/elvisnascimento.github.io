017cee62d02447f971794e98192e0607af356f51
Elvis Nascimento <elvisnascimentodev@gmail.com>
2025-02-22 09:14:57 -0300

    Curriculo Atualizado

---
+++ script.js
@@ -0,0 +1,7 @@
+var d = new Date();
+var n = d.getFullYear();
+document.write(n);
+document.addEventListener('DOMContentLoaded', function() {
+  var data_nascimento = document.getElementById("data_nascimento").value;
+  console.log(data_nascimento);
+});
